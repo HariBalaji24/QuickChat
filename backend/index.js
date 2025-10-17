1e7419565b22dddcdc7d30560182d0170eb1b14d
HariBalaji24 <sharibalaji2006@gmail.com>
2025-10-17 19:09:55 +0530

    cors changed 2

--- backend/index.js
+++ backend/index.js
@@ -8,12 +8,20 @@ import { Server } from "socket.io";
 //create http server
 const app = express();
 const server = http.createServer(app);
-app.use(cors());
+
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://quick-chat-lemon.vercel.app"
+  ],
+  credentials: true,
+}));
+
 
 //initialize socket.io
 export const io = new Server(server, {
   cors: { origin: "http://localhost:5173",
-  origin:"https://https://quick-chat-lemon.vercel.app", // frontend origin
+  origin:"https://quick-chat-lemon.vercel.app", // frontend origin
   credentials: true, },
 });
 
@@ -56,4 +64,4 @@ app.use("/", userrouter);
 app.use((req, res, next) => {
   res.status(404).json({ message: `cant find ${req.originalUrl}` });
 });
-server.listen(process.env.PORT, () => {});
+server.listen(3000, () => {console.log(`✅ Server running on port`);});
