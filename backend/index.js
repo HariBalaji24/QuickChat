968ad4b9e8c61519742fb2277946e656240987d1
HariBalaji24 <sharibalaji2006@gmail.com>
2025-10-19 16:37:07 +0530

    frontend url changed

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const server = http.createServer(app);
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://quick-chat-lemon.vercel.app"
+    "https://quick-chat-eight-green.vercel.app/"
   ],
   credentials: true,
 }));
@@ -23,7 +23,7 @@ export const io = new Server(server, {
   cors: {
     origin: [
       "http://localhost:5173",
-      "https://quick-chat-lemon.vercel.app"
+      "https://quick-chat-eight-green.vercel.app/"
     ],
     credentials: true,
   },
